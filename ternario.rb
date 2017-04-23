ece9583ea03b7a7a59f465a2688ca7ff6b38f445
Turiano <romero.guzman18@gmail.com>
2017-04-23 12:31:18 -0500

    Ternario

---
+++ ternario.rb
@@ -0,0 +1,33 @@
+class Ternario 
+	def initialize
+		
+	end
+	
+	def inicio
+		 
+		 user = "JUan"
+       
+
+		 #if user == "Turi"
+		 #	puts "Eres El Maestro"
+		 #else
+		 #	puts "Eres Un Alumno" 	
+		 #end
+          
+         #puts (if user == "Turi" then "Maestro" else "Alumno" end)
+             
+         # respuesta = if user == "Turi" then
+         # 	"Maestro"
+         # else
+         #	"Alumno"
+         # end
+         # puts respuesta
+
+         puts user == "Turi" ? "Maestro" : "Alumno"
+           
+	end
+	
+end
+
+objeto = Ternario.new
+objeto.inicio
