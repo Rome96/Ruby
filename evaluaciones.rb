493d360a920bd6d143bc9cf1b4127f2d99cdbfbf
Turiano <romero.guzman18@gmail.com>
2017-04-26 20:40:11 -0500

    Creacion de nuevo proye

---
+++ evaluaciones.rb
@@ -0,0 +1,17 @@
+print "Dame Tu Calificacion (1-5): "
+calificacion = gets.chomp.to_i
+
+if calificacion == 5 || calificacion == 4
+	puts "Muy Bieen!"
+
+elsif calificacion == 3.5 || 3
+	puts "Bien, Pero Aun Puedes Mejorar"
+
+elsif calificacion == 2
+	puts "Vas Maaaaallllllll"
+
+else
+	puts "Te Llevo El Diablo"
+
+end
+	
