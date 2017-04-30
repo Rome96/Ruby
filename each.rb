3c689ecfe5e830a1d0b17ce504507b74a6e13541
Turiano <romero.guzman18@gmail.com>
2017-04-29 19:24:21 -0500

    update metodo length

--- each.rb
+++ each.rb
@@ -1,4 +1,6 @@
 # ciclo each
+#with_index para mirar las posiciones de los arreglos
+# metodo length para ver la cantidad de elementos del arreglo
 
 class Ciclo 
 	def initialize()
@@ -7,17 +9,19 @@ class Ciclo
 
 	def ejemplo
 		
-		calificaciones = %w[5 4 3 2 1]
+		calificaciones = %w[5 4 4 5 4.5]
 
 		suma = 0
 
-		calificaciones.each_with_index do |calificacion, posicion|
-			puts "En La Posicion #{posicion} Temenos: #{calificacion}"
+		calificaciones.each_with_index do |calificacion, posicion| 
+			#puts "En La Posicion #{posicion} Temenos: #{calificacion}"
+			suma += calificacion.to_i
 		end
+
+		puts "El Promedio De Tus calificaciones Es #{suma.to_f / calificaciones.length}"
 			
 		end
 	end
 
 objeto = Ciclo.new
 objeto.ejemplo
-gets()
