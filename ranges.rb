c0dac0115fb79188c351a1a27edfe92a0eda02b5
Turiano <romero.guzman18@gmail.com>
2017-05-06 14:32:28 -0500

    contar de 2 en 2 con el metodo .step(numero)

--- ranges.rb
+++ ranges.rb
@@ -6,7 +6,12 @@ class Ranges
 		
 		# numeros = [1,2,3,4,5,6,7,8,9,10]
         # numeros = (1..10)
-       (1..20).each do |numero|
+
+       # (1..10).each do |numero| contar del 1 al 10
+       #  	puts numero
+
+
+       (0..20).step(2).each do |numero| # para contar de 2 en 2
         	puts numero
 	end
 	
