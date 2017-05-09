0e263933c7ec0c0835b0feadf2926bc686a3fc89
Turiano <romero.guzman18@gmail.com>
2017-05-08 19:42:46 -0500

    ejemplo con el siclo until, el numero magico

--- S_Until.rb
+++ S_Until.rb
@@ -3,7 +3,18 @@ class SUntil
 	end
 	
 	def ejemplo()
-		
+
+		numero_magico = 17
+          print "Adivina Cual Es El Numero Magico XD: "
+		numero_usuario = gets().chomp.to_i
+
+		until numero_magico == numero_usuario
+	         print "Numero Incorrecto, Adivina Otra Vez XD: "
+		     numero_usuario = gets().chomp.to_i	
+		end
+
+		print "Felicidades Adivinastes, Sos Un Capo Isaac :)  "
+		gets()
 	end
 	
 end
