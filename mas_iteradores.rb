80b9c91eb9eaadb54eaad5dbda8e14556622569a
Turiano <romero.guzman18@gmail.com>
2017-05-09 21:39:26 -0500

    imprimir del 1-10 con el iterador .times

--- mas_iteradores.rb
+++ mas_iteradores.rb
@@ -1,9 +1,16 @@
+#time, upto, downto
+
 class MasIteradores 
 	def initialize()
 	end
 	
 	def ejemplo
-		
+
+#imprimir del 1 - 10
+	   10.times do |i|
+	   	puts i + 1
+	   	end
+#-------------------------------------------------------
 	end
 end
 objeto = MasIteradores.new
