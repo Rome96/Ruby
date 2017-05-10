db9ea27b8b0ecbf5f9d443ea601a8f6df20f171a
Turiano <romero.guzman18@gmail.com>
2017-05-09 21:44:31 -0500

    imprimir mensaje 10veces (.times)

--- mas_iteradores.rb
+++ mas_iteradores.rb
@@ -7,10 +7,16 @@ class MasIteradores
 	def ejemplo
 
 #imprimir del 1 - 10
-	   10.times do |i|
-	   	puts i + 1
+	   # 10.times do |i|
+	   # 	puts i + 1
+	   # 	end
+#imprimir mensaje 10 veces
+	   10.times do 
+	   	puts "me ejecutare 10 veces"
 	   	end
 #-------------------------------------------------------
+	   
+
 	end
 end
 objeto = MasIteradores.new
