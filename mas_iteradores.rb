270d7899dae306386a96689f9e9e50681322abaa
Turiano <romero.guzman18@gmail.com>
2017-05-09 21:47:40 -0500

    imprimir del 1-10 con el iterador .upto

--- mas_iteradores.rb
+++ mas_iteradores.rb
@@ -6,16 +6,20 @@ class MasIteradores
 	
 	def ejemplo
 
-#imprimir del 1 - 10
+#imprimir del 1 - 10 con times
 	   # 10.times do |i|
 	   # 	puts i + 1
 	   # 	end
 #imprimir mensaje 10 veces
-	   10.times do 
-	   	puts "me ejecutare 10 veces"
-	   	end
+	   # 10.times do 
+	   # 	puts "me ejecutare 10 veces"
+	   # 	end
 #-------------------------------------------------------
-	   
+	   #imprimir del 1 - 10 con upto
+
+	   1.upto(10) do |i|
+	   	puts i
+	   end
 
 	end
 end
