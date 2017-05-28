ae950eb09aa05e808a0e941151dbd6bf485cf35b
Turiano <romero.guzman18@gmail.com>
2017-05-28 15:11:38 -0500

    imprimir la diagonal de una matriz

--- matrices.rb
+++ matrices.rb
@@ -3,18 +3,25 @@ class Matrices
 	end
 
 	def ejercicio
-
+#---------------------------------------------------------
 # [
 # 	3 5 6
 # 	4 8 5
 # 	7 6 3
 # ]
+		# require 'matrix'
+		# matriz = Matrix[[3,5,6], [4,8,5], [7,6,3]]  # Matriz funcional
+		# matriz.each do |i|
+  #        puts i
+		# end  
+#-------------------------------------------------------
 
-		require 'matrix'
-		matriz = Matrix[[3,5,6], [4,8,5], [7,6,3]]
-		matriz.each do |i|
+	    require 'matrix'
+		matriz = Matrix[[3,5,6], [4,8,5], [7,6,3]]  #Imprimir la diagonal de una matriz
+		matriz.each(:diagonal) do |i|
          puts i
 		end
+
 
 	end
 	
