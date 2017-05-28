ca98ed24994c754ea409a88d4090f4b9753c4c10
Turiano <romero.guzman18@gmail.com>
2017-05-28 15:40:05 -0500

    ternimado hash

--- hash.rb
+++ hash.rb
@@ -4,7 +4,11 @@ class Ejer
 
 	def ejerciocio
 		
-		
+		programador = {name: "Turiano Romero", edad: "21", computer: 3}
+
+		programador.each do |clave, valor|
+              puts "En #{clave} Esta Guardado #{valor}"
+		end
 
 	end
 end
