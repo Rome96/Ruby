1e399c285ba2471e179dfa8d99d68fb1e517ea13
Turiano <romero.guzman18@gmail.com>
2017-06-03 18:56:30 -0500

    utilizando el metodo keys, que muestra todas las claves del has

--- operaciones_con_hash.rb
+++ operaciones_con_hash.rb
@@ -1,5 +1,6 @@
 #el metodo size o length sirve para mirar cantidades
-# el metodo has_key nos devuelve verdadedo cuando pasamos una clave que existe en el has
+# el metodo has_key? nos devuelve verdadedo cuando pasamos una clave que existe en el has
+# el metodo keys sirve para mostrar todas las claves del has
 
 class OperacionesConHash 
 	def initialize()
@@ -9,7 +10,8 @@ class OperacionesConHash
 
 		programador = {name: "Turiano Romero", edad: "21", computer: 5}
 		# puts programador.length
-		 puts programador.has_key?(:edad)
+		#puts programador.has_key?(:edad)
+		 puts programador.keys
 
 		
 	end
