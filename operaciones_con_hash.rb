3591007711a9d610df63ff187945945c979e7f71
Turiano <romero.guzman18@gmail.com>
2017-06-03 19:02:15 -0500

    utilizando el metodo delete

--- operaciones_con_hash.rb
+++ operaciones_con_hash.rb
@@ -2,6 +2,7 @@
 # el metodo has_key? nos devuelve verdadedo cuando pasamos una clave que existe en el has
 # el metodo keys sirve para mostrar todas las claves del has
 # elmetodo values nos muestra el puro contenido de las claes
+# ""   ""  delete para eliminar una clave especifica del has
 class OperacionesConHash 
 	def initialize()
 	end
@@ -12,8 +13,9 @@ class OperacionesConHash
 		# puts programador.length
 		#puts programador.has_key?(:edad)
 		 #puts programador.keys
-		 puts programador.values
 
+		 programador.delete(:edad)
+         puts programador
 		
 	end
 	
