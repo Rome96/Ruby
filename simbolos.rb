900d943f8d0ce15853a8f2b068a2f9e25f2e01c0
Turiano <romero.guzman18@gmail.com>
2017-06-05 22:15:58 -0500

    update utilizando .object_id

--- simbolos.rb
+++ simbolos.rb
@@ -1,9 +1,21 @@
+# Los Simbolos son una cadena inmutable
+
 class Simbolos
 	def initialize()
 	end
 	
 	def ejemplo
-		
+		cadena = "Turiano"
+        cadena2 = "Turiano"
+
+        simbolo = :Turiano
+        simbolo2 = :Turiano
+
+        puts cadena.object_id
+        puts cadena2.object_id
+
+        puts simbolo.object_id
+        puts simbolo2.object_id
 	end
 	
 end
