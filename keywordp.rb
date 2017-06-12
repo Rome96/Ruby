64457e9b5b28381ee9a528429637cdb047b0301d
Turiano <romero.guzman18@gmail.com>
2017-06-11 20:00:03 -0500

    imprimir si es senior o joven

--- keywordp.rb
+++ keywordp.rb
@@ -1,3 +1,12 @@
-def hola
+def hola (nombre:,edad:0,**options)
+	if edad > 30
+		puts "Hola Señor #{nombre}"
+	elsif edad < 30
+		puts "Hola Joven #{nombre}"
+	end
+
+	puts options[:animal_favorito]
 	
 end
+
+hola(edad:21,nombre:"Turiano",color_favorito:"azul",animal_favorito:"Perro")
