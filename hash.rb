f62a4326bca814ad792d6c68f429b58de7835de2
Turiano <romero.guzman18@gmail.com>
2017-06-25 20:10:13 -0500

    update

--- hash.rb
+++ hash.rb
@@ -3,7 +3,6 @@ class Ejer
 	end
 	def ejerciocio
 		programador = {name: "Turiano Romero", edad: "21", computer: 3}
-
 		programador.each do |clave, valor|
               puts "En #{clave} Esta Guardado #{valor}"
 		end
