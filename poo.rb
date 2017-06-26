41ab8a0855957d38c17b8327386160de1431a8e7
Turiano <romero.guzman18@gmail.com>
2017-06-25 20:21:30 -0500

    utilizando atributos

--- poo.rb
+++ poo.rb
@@ -14,3 +14,9 @@ end
 
 curso_de_Ruby = Video.new
 curso_de_Ruby.title = "Objetos Y Clases"
+
+curso_de_Ruby2 = Video.new
+curso_de_Ruby2.minutes = "Atributos"
+
+puts curso_de_Ruby.title
+puts curso_de_Ruby.minutes
