ce2dd97d3b10b46a8bae39000b39c42ff8e226f9
Turiano <romero.guzman18@gmail.com>
2017-06-27 10:01:33 -0500

    Creacion del metodo initialize

--- poo.rb
+++ poo.rb
@@ -1,20 +1,19 @@
 class Video
   attr_accessor :minutes, :title
-  
+
   def play
   end
-  
+
   def pause
   end
-  
+
   def stop
   end
-  
+
 end
 
 curso_de_Ruby = Video.new
 curso_de_Ruby.title = "Objetos Y Clases"
-
 curso_de_Ruby2 = Video.new
 curso_de_Ruby2.minutes = "Atributos"
 
