4e4d1efa47af5c7594767d1f8d9921799284ba2c
Turiano <romero.guzman18@gmail.com>
2017-06-27 10:21:00 -0500

    utilizando variables de instancia e imprimiendo resultados

--- propiedades.rb
+++ propiedades.rb
@@ -2,6 +2,14 @@ class Tutor
 
   def initialize(name)
     @nombre = name #variables de instancia
+
+    puts @nombre
   end
 
 end
+
+turiano = Tutor.new("Turiano")
+jesus = Tutor.new("Jesus")
+
+# puts turiano.nombre
+# puts jesus.nombre
