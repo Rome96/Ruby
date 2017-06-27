700f44b3c4490a8484f6470b81a34a349b39b93e
Turiano <romero.guzman18@gmail.com>
2017-06-27 10:05:36 -0500

    pasandole parametros al metodo initialize

--- poo.rb
+++ poo.rb
@@ -1,6 +1,10 @@
 class Video
   attr_accessor :minutes, :title
 
+  def initialize (title)
+    self.title = title
+  end
+
   def play
   end
 
@@ -12,10 +16,10 @@ class Video
 
 end
 
-curso_de_Ruby = Video.new
-curso_de_Ruby.title = "Objetos Y Clases"
-curso_de_Ruby2 = Video.new
-curso_de_Ruby2.minutes = "Atributos"
-
-puts curso_de_Ruby.title
-puts curso_de_Ruby.minutes
+curso_de_Ruby = Video.new("Objetos Y Clases")
+# curso_de_Ruby.title = "Objetos Y Clases"
+# curso_de_Ruby2 = Video.new
+# curso_de_Ruby2.minutes = "Atributos"
+#
+# puts curso_de_Ruby.title
+# puts curso_de_Ruby.minutes
